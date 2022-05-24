255db0442e95d5ae78f272cf84f78acaa15ebc4e
5065 Jean <jean5065@adcrow.tech>
2022-05-24 11:43:57 +0800

    feat: console log all git-rev-sync methods

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -2,17 +2,14 @@ const git = require('git-rev-sync')
 
 // fail if the git command is not found in PATH
 
-const currentTag = '0.0.1_git-rev-sync'
-const currentBranch = 'gitRevSync'
-console.log(`Tag: ${git.tag([currentTag])}`)
-// return the current tag and mark as dirty if markDirty is truthful; this method will fail if the git command is not found in PATH
-// git.short([filePath], [length])
-console.log(`Current Head: ${git.long()}`)
-console.log(`Current Branch: ${git.branch()}`)
+console.log(`Tag: ${git.tag(null)}`)
+console.log(`Short Head Commit: ${git.short(null, [10])}`)
+console.log(`Long Head Commit: ${git.long(null)}`)
+console.log(`Current Branch: ${git.branch(null)}`)
 console.log(`Total Commits: ${git.count()}`)
 console.log(`Commit Date: ${git.date()}`)
 console.log(`Unstaged Changes: ${git.hasUnstagedChanges()}`)
 console.log(`Uncommitted Changes: ${git.isDirty()}`)
-// console.log(git.isTagDirty(currentTag))
+console.log(`Dirty Tag: ${git.isTagDirty(null)}`)
 console.log(`Commit Message: ${git.message()}`)
 console.log(`Remote Url: ${git.remoteUrl()}`)
