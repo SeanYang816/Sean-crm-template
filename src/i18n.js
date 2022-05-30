c05dc52fbbe483b16978c66531eee75e8fcf9f0d
5065 Jean <jean5065@adcrow.tech>
2022-05-30 18:37:40 +0800

    feat: decides language display based on query string

--- src/i18n.js
+++ src/i18n.js
@@ -2,6 +2,7 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
 import { LANGUAGE } from 'constants'
+import qs from 'qs'
 
 i18n
   .use(Backend)
@@ -10,7 +11,7 @@ i18n
     backend: {
       loadPath: '/locales/{{lng}}.json',
     },
-    lng: LANGUAGE.ZH_TW, // 預設語言
+    lng: qs.parse(window.location.search, { ignoreQueryPrefix: true }).language || LANGUAGE.ZH_TW, // 預設語言
     fallbackLng: LANGUAGE.EN, // 如果當前切換的語言沒有對應的翻譯則使用這個語言，
     interpolation: {
       escapeValue: false,
